449f92574933cd3f32c44ddbe4964e4c529a6cbf
chaymaa <lamini.chaymaa@gmail.com>
2026-02-16 11:51:17 +0100

    chore:initialize Spring Boot Maven Project

---
+++ src/main/java/com/example/demo/Equipe1BackendSprinApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Equipe1BackendSprinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Equipe1BackendSprinApplication.class, args);
+	}
+
+}
